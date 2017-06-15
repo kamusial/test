8e52930ec8c50213b3a1c41cf74a3d8780de20cb
kamusial <kamil.support@gmail.com>
2017-06-15 07:34:17 +0200

    wywalone klasy- startup.h problem

--- mainGenetyczny/functions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-/*
-struct operacja;
-struct zamowienie;
-void licz_zapotrzebowanie(vector<operacja>& procLL_V1,
-	vector<operacja>& procLL_V2,
-	vector<operacja>& procRL_V1,
-	vector<operacja>& procRL_V2,
-	vector<zamowienie>& zam);*/
